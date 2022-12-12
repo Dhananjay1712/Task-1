a938f3af149555dd8c87832ec1d2f9e706ca19e8
Dhananjay Rangat <dhananjayrangat143@gmail.com>
2022-12-12 19:47:38 +0530

    adding diff.java

---
+++ diff.java
@@ -0,0 +1,9 @@
+public class diff {
+    public static void main(String[] args) {
+        int a,b,c;
+        a = 10;
+        b = 20;
+        c = b - a;
+        System.out.println("The difference between two numbers is : "+ c);
+    }
+}
