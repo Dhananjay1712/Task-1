5cbd5c162367d09d177154305ff3926f10290173
Dhananjay Rangat <dhananjayrangat143@gmail.com>
2022-12-12 20:01:16 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a,b,c;
+        a = 2;
+        b = 4;
+        c = a * b;
+        System.out.println("The multiplication of two numbers is : "+ c);
+    }
+}
