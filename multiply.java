38db55ee523d7ddf136d88beb7c3c50bb36a6835
Dhananjay1712 <119772699+Dhananjay1712@users.noreply.github.com>
2023-02-16 21:44:35 +0530

    Delete multiply.java

--- multiply.java
+++
@@ -1,9 +0,0 @@
-public class multiply {
-    public static void main(String[] args) {
-        int a,b,c;
-        a = 2;
-        b = 4;
-        c = a * b;
-        System.out.println("The multiplication of two numbers is : "+ c);
-    }
-}
